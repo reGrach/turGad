38f5b05345f64aa63c156297e8b819c7c0ae4ae1
German <GermanGrach.94@ya.ru>
2019-10-26 21:04:14 +0300

    add setup and update gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='turGad',
+    version='0.1.0',
+    packages=['tg_back'],
+    url='https://github.com/reGrach/turGad',
+    license='',
+    author='reGrach',
+    author_email='germangrach.94@ya.ru',
+    description='Project for the game "Turgad"'
+)
