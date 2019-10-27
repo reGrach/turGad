4dcd3facaaef5adcf3f4163e5a51338de6b21039
MaxFaber <makssup32@gmail.com>
2019-10-27 14:42:00 +0300

    data_url_byMax

--- tg_back/api.py
+++ tg_back/api.py
@@ -4,3 +4,13 @@ from tg_back import tur_app
 @tur_app.route('/registration')
 def registration():
     return 'упсешная геристраияц'
+
+@tur_app.route('/helo')
+def login():
+    from flask import request
+
+    with app.test_request_context('/hello', method='POST'):
+        # теперь, и до конца блока with, вы можете что-либо делать
+        # с контекстом, например, вызывать простые assert-ы:
+        assert request.path == '/hello'
+        assert request.method == 'POST'
