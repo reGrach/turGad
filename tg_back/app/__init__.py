d0d1023a73a2bf47b6364b2f46b1e24c72bc70d6
German <GermanGrach.94@ya.ru>
2019-10-30 14:48:07 +0300

    добавил регистрацию

--- tg_back/app/__init__.py
+++ tg_back/app/__init__.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 
 
 tur_app = Flask(__name__)
+tur_app.config['JSON_AS_ASCII'] = False
 CORS(tur_app)
 
 from app.api import team, stage
--- tg_back/app/api/team.py
+++ tg_back/app/api/team.py
@@ -1,13 +1,28 @@
 from app import tur_app, db
-from flask import jsonify
+from flask import jsonify, request
 
 
-@tur_app.route('/team/qr/<int:team_id>')
-def registration(team_id):
+@tur_app.route('/team/registration', methods=['POST'])
+def registration():
+    data = request.get_json()
+    res = db.registration_team(data)
+    return res.__str__()
+
+
+
+@tur_app.route('/team/get/<int:team_id>')
+def get_team_name(team_id):
     # Делаем запрос в БД
     team = db.get_team_by_id(team_id)
-    if team == {}:
-        return {}
+    if team == '':
+        return jsonify({'result': False})
     else:
-        return jsonify(team)
+        return jsonify({'result': True, 'title': team})
+
+
+@tur_app.route('/mark')
+def mark():
+    # Делаем запрос в БД
+    team = db.get()
+    return jsonify(team)
 
