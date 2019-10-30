c84c1f3a7b2f61a8680952c9a76606469f44bd60
German <GermanGrach.94@ya.ru>
2019-10-30 18:43:19 +0300

    настройка ip внешнего

--- tg_front/src/plugins/axios.js
+++ tg_front/src/plugins/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 Vue.use({
     install (Vue) {
         Vue.prototype.$api = axios.create({
-            baseURL: 'http://127.0.0.1:5000/',
+            baseURL: 'http://84.201.184.131:5000/',
             timeout: 1000,
         })
     }
