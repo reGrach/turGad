9303190eea22ffc634394e7af3a750c37fe6bd8f
German <GermanGrach.94@ya.ru>
2019-11-02 18:42:21 +0300

    tiny fix

--- tg_back/app/__init__.py
+++ tg_back/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask
 from flask_cors import CORS
 from app import config
 
@@ -7,6 +7,5 @@ tur_app = Flask(__name__)
 tur_app.config['JSON_AS_ASCII'] = False
 tur_app.config['SECRET_KEY'] = config.SECRET_KEY
 CORS(tur_app)
-
 
 from app.api import team, stage
