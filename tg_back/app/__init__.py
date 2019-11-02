41610045c53c06ea69dfe16e482ae3088905f56d
German <GermanGrach.94@ya.ru>
2019-11-02 20:08:48 +0300

    tiny back fix

--- tg_back/app/__init__.py
+++ tg_back/app/__init__.py
@@ -1,12 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
-from app import config
-from .api import api
+from app import config, api
 
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(api, url_prefix="/api")
+    app.register_blueprint(api.api, url_prefix="/api")
     app.config['JSON_AS_ASCII'] = False
     app.config['SECRET_KEY'] = config.SECRET_KEY
     CORS(app)
