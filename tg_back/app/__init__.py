c3ece006aec16e843cae0c28e625bfada00f67fd
German <GermanGrach.94@ya.ru>
2019-11-02 14:38:40 +0300

    fix cors

--- tg_back/app/__init__.py
+++ tg_back/app/__init__.py
@@ -9,15 +9,4 @@ tur_app.config['SECRET_KEY'] = config.SECRET_KEY
 CORS(tur_app)
 
 
-@tur_app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    if request.method == 'OPTIONS':
-        response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
-        headers = request.headers.get('Access-Control-Request-Headers')
-        if headers:
-            response.headers['Access-Control-Allow-Headers'] = headers
-    return response
-
-
 from app.api import team, stage
