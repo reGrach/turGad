4f6891995e17c8c754df76003033d3fe4255be41
German <GermanGrach.94@ya.ru>
2019-11-03 05:29:27 +0300

    к предыдущему

--- tg_front/src/axios/index.js
+++ tg_front/src/axios/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://127.0.0.1:5000/api',
-    // baseURL: 'http://84.201.184.131:5000/api',
+    // baseURL: 'http://127.0.0.1:5000/api',
+    baseURL: 'http://84.201.184.131:5000/api',
     timeout: 10000
 });
 
